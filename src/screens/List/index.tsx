7de04957ec942e2664d6c49f58f297ae83f1766a
MARCIA REJANE RODRIGUES DE SOUZA <a95434868@rn.senac.local>
2025-07-01 14:44:25 -0300

    Questão 2 finalizada

--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,16 +1,27 @@
-{/* Minha tela que ira aparecer.
-  importei os componentes que foi pedido
-  e importei o styles, que é a parte de estilização.
-  */}
-import { Text, View } from 'react-native';
+{/*Foi feito o input usando useState que armazena o valor e mostra o valor salvo em texto */}
+import React, {useState} from 'react';
+import { Text, View, TextInput } from 'react-native';
 
 import {styles} from './styles';
 
-{/* Exportei minha tela para o App.tsx */}
+
 export default function List() {
+  const [texto, setTexto] = useState('');
+ 
+  
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Minhas tarefas</Text>
+
+      <TextInput
+        style={styles.Input}
+        placeholder='Digite uma tarefa'
+        placeholderTextColor={'#B19cD9'}
+        onChangeText={setTexto}
+        value={texto}
+
+      />
+      <Text style={styles.texto}>{texto}</Text>
     </View>
   );
-}
+  }
--- src/screens/List/styles.ts
+++ src/screens/List/styles.ts
@@ -1,4 +1,3 @@
-{/* importei o stylesheet para fazer a estilização da tela.*/}
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
@@ -15,4 +14,19 @@ export const styles = StyleSheet.create({
     textShadowColor: '#e0b0ff',
     textShadowOffset: {width: 2, height: 2},
   },
+  Input:{
+    backgroundColor: 'white',
+    height: 56,
+    width: 250,
+    borderRadius: 5,
+    fontWeight: 'bold',
+    padding:10,
+    
+  },
+  texto: {
+    fontSize: 20,
+    color: 'black',
+    textShadowColor: '#e0b0ff',
+    textShadowOffset: {width: 2, height: 2},
+  }
 });
